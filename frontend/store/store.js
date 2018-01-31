89afa0c1e4b3c734aa147fd01ce4d77f5b451c3f
Lawrence <lawrenccee@gmail.com>
2018-01-30 22:22:11 -0800

    Prepare for initial heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger'; // TODO REMOVE FOR PRODUCTION
+// import logger from 'redux-logger'; // TODO REMOVE FOR PRODUCTION
 import thunk from '../thunk/thunk';
 
 const configureStore = (preloadedState) => {
   const store = createStore(
     rootReducer, 
     preloadedState, 
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   );
 
   return store;
