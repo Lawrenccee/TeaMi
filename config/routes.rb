eb64f14cf31dc58d6ced188649fddf1d87b6c884
Lawrence <lawrenccee@gmail.com>
2018-02-01 12:34:16 -0800

    Add redis gem and add routes for action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
